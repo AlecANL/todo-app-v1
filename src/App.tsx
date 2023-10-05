dcb0bfe4a81f01a0ea54399556583f00f0ce734d
alexei <alexeialvarez4@gmail.com>
2023-10-04 20:56:40 -0600

    feat: add change bg image 💡

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { useToggleTheme } from './hooks/useToggleTheme.ts'
+import { BgImageContainer } from './app.styled.tsx'
 
 function App () {
   const { handleToggleTheme } = useToggleTheme()
   return (
     <>
-      <h1>Hello</h1>
+      <BgImageContainer className='bg-image'></BgImageContainer>
       <button id='theme-toggle' onClick={handleToggleTheme}>Change theme</button>
     </>
   )
---
+++ src/app.styled.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components'
+
+export const BgImageContainer = styled.div`
+  width: 100%;
+  min-height: clamp(12.5rem, 11.186vw + 5.327rem, 18.75rem);
+`
